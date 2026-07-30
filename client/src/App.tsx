6b85c13f793de754667e1d46403ddf4b4e1df602
Kumar <kumararaja.kt@gmail.com>
2026-07-30 09:05:11 +0530

    added transaction page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import AppSidebar from "./Sidebar";
+import TransactionPage from "./pages/TransactionPage";
 
 function App() {
-  const [selectedMenu, setSelectedMenu] = useState("home");
+  const [selectedMenu, setSelectedMenu] = useState("dashboard");
 
   return (
     <>
       <AppSidebar selectedMenu={selectedMenu} setSelectedMenu={setSelectedMenu}>
-        <></>
+        {selectedMenu === "transaction" && <TransactionPage />}
       </AppSidebar>
     </>
   );
